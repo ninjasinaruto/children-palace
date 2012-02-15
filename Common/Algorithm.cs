c9558ec90616be32bbb8e73158bbc0b04315ad44
gyuzhou <gyuzhou@gmail.com>
2012-02-15 18:26:46 +0000

    创建项目

---
+++ Common/Algorithm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data;
+
+namespace Common
+{
+    public class Algorithm
+    {
+        public static int GetTablePositionFromDataGridViewRow(DataGridViewRow row, DataRowCollection rows)
+        {
+            DataRowView drv = row.DataBoundItem as DataRowView;
+            return rows.IndexOf(drv.Row);
+        }
+    }
+}
